3a8687802068b800e318a0b073902c3028377c29
DaYrkann <69507009+DaYrkann@users.noreply.github.com>
2021-04-20 20:45:47 +0400

    Update mApp.php

--- assets/scripts/php/modeles/mApp.php
+++ assets/scripts/php/modeles/mApp.php
@@ -1,12 +1,22 @@
 <?php
 	require 'assets/scripts/php/bdd.php';
-
+	/**
+	* BindParam d'array
+	* @param {Object} $poStatement
+	* @param {Array} $paAarray
+	* 
+	*/
 	function PDOBindArray(&$poStatement, &$paArray) {
 		foreach ($paArray as $k=>$v) {
 			@$poStatement->bindValue(':'.$k,$v);
 		}
 	}
 
+	/**
+	* Récupérer les données demandé.
+	* @param {String} $data
+	* @return $return Un tableau associatif des données demandés.
+	*/
 	function allData(string $data) {
 		$bdd = connectBDD();
 		$return = array();
@@ -17,10 +27,22 @@
 		return $return;
 	}
 
+	/**
+	* Créer un nouvel utilisateur ou un poste dans la base de données.
+	* @param {String} $section
+	* @param {String} $data1
+	* @param {String} $data2
+	* @param {String} $data3
+	* @param {String} $data4
+	* @param {Boolean} true/false
+	*/
 	function createData(string $section, string $data1, string $data2, string $data3, string $data4) {
 		try {
 			$bdd = connectBDD();
-			$stm = $bdd->prepare("INSERT INTO $section VALUES (:id, :value1, :value2, :value3)");
+			$stm = $bdd->prepare("
+									INSERT INTO $section VALUES 
+										(:id, :value1, :value2, :value3)
+								");
 			$data = array(
 				'id' => $data1,
 				'value1' => $data2,
@@ -35,11 +57,49 @@
 		}
 		
 	}
-
+	/**
+	* Suprimer un utilisateur ou un poste de la base de donnée.
+	* @param {String} $section
+	* @param {String} $code
+	*/
 	function deleteData(string $section, string $code) {
 		$bdd = connectBDD();
-		$stm = $bdd->prepare("DELETE FROM $section where id = '$code'");
-		var_dump($stm);
-		exit();
+		$stm = $bdd->prepare("DELETE FROM $section where id = :code ");
+		$stm->bindParam(':code', $code);
 		$stm->execute();
+	}
+	/**
+	* Enregistre une attribution de créneau horaire dans la base de donnée et change la disponibilité d'une poste.
+	* @param {String} $user
+	* @param {String} $comp
+	* @param {String} $time
+	*/
+	function attribTime($jour,string $user, string $comp, string $time) {
+		$bdd = connectBDD();
+		$stm = $bdd->prepare("
+								INSERT INTO attribution VALUES
+									(:jour, :user, :comp, :attrib)
+							");
+		$data = array(
+			'jour' => $jour,
+			'user' => $user,
+			'comp' => $comp,
+			'attrib' => $time
+
+		);
+		PDOBindArray($stm,$data);
+		$stm->execute();
+	}
+	/*
+	* Supprimer une attribution de creneau horaire de la base de données.
+	* @param {String} $jour
+	*/
+	function cancelTime(string $jour) {
+		$bdd = connectBDD();
+		$stm = $bdd->prepare("
+								DELETE FROM attribution
+									WHERE Creneau = :creneau
+							");
+		$stm->bindParam(':creneau', $jour, PDO::PARAM_STR);
+		$stm = $bdd->execute();
 	}
